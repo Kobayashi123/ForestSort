409c8663503be3bd7c726cdc25aa1689e305f3d4
kobayashi shun <g2053417@cc.kyoto-su.ac.jp>
2022-07-05 13:51:28 +0900

    エラーno

--- Forest_Program/forest/ForestView.java
+++ Forest_Program/forest/ForestView.java
@@ -7,24 +7,29 @@ import mvc.View;
 /**
  * 樹状整列におけるMVCのビュー（V）を担うクラス。
  */
-public class ForestView extends View {
+public class ForestView extends View
+{
 
 	/**
 	 * このクラスのインスタンスを生成するコンストラクタ。
 	 */
-	public ForestView(ForestModel aModel) {
+	public ForestView(ForestModel aModel)
+	{
+		super(aModel);
 	}
 
 	/**
 	 * このパネル（ビュー）の描画が必要になったときに動作するメソッド。
 	 */
-	public void paintComponent(Graphics aGraphics) {
+	public void paintComponent(Graphics aGraphics)
+	{
 	}
 
 	/**
 	 * 指定された位置（座標）にノードが存在するかを調べるメソッド。
 	 * もしも見つからなかった場合には、nullを応答。
 	 */
-	public void whichOfNodes(Point aPoint) {
+	public void whichOfNodes(Point aPoint)
+	{
 	}
 }
